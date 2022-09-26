e0eb9701a1859aa3888d7babde9c37233fb4c199
DaviNatan10 <112419442+DaviNatan10@users.noreply.github.com>
2022-09-26 16:21:46 -0300

    telas relatorio ilustrativas

--- PROJETO.c
+++ PROJETO.c
@@ -28,6 +28,13 @@ void cadastrar_animal(void);
 void editar_animal(void);
 void pesquisar_animal(void);
 void delete_animal(void);
+
+//Funcoes Mod Relatorios
+void modulo_relatorio(void);
+char menu_relatorio(void);
+void relatorio_usuarios(void);
+void relatorio_animal(void);
+
 // Começo do Programa.
 int main(void) {
     char opcao;
@@ -540,3 +547,82 @@ void delete_animal(void){
     printf("\n");
 
 }
+
+void modulo_relatorio(void) {
+    char opcao;
+    do {
+        opcao = menu_relatorio();
+        switch(opcao) {
+            case '1': 	relatorio_usuario();
+                        break;
+            case '2': 	relatorio_animal();
+                        break;
+           
+        } 		
+    } while (opcao != '0');
+}
+
+char menu_relatorio(void) {
+    char op;
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///         SIG-Pet um sistema de agendamento de consultas para pets            ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///            = = = = sistema de agendamento de consulta para pets = = = =     ///\n");
+    printf("///           |                                                             |   ///\n");
+    printf("///           |           1. Relatorios de Usuarios                         |   ///\n");
+    printf("///           |           2. Relatorio do Animal                            |   ///\n");
+    printf("///           |           0. Voltar ao Menu Principal                       |   ///\n");
+    printf("///           |                                                             |   ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =    ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("escolha oque deseja:\n");
+    scanf("%c",&op);
+    getchar();
+    printf("\n");
+    printf("\t\t\t<<< ... Loading ... >>>\n");
+    sleep(1);
+
+    return op;
+}
+void relatorio_usuario(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///            = = = Sistema de agendamento de consultas para pet = = =         ///\n");
+    printf("///           |     Relatorio Animal                                    |       ///\n");
+    printf("///           |     Estara Disponivel em Breve                          |       ///\n");
+    printf("///           |     Quando Nosso Professor                              |       ///\n");
+    printf("///           |     Flavius Gorgonio                                    |       ///\n");
+    printf("///           |     Ensinar Nos Meros Humanos                           |       ///\n");
+    printf("///           |     Armazenar Dados.                                    |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = = = =        ///\n");
+    printf("///                                                                             ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+} 
+
+void relatorio_animal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                             ///\n");
+    printf("///            = = = Sistema de agendamento de consultas para pet = = =         ///\n");
+    printf("///           |     Relatorio de Usuario                                |       ///\n");
+    printf("///           |     Estara Disponivel em Breve                          |       ///\n");
+    printf("///           |     Quando Nosso Professor                              |       ///\n");
+    printf("///           |     Flavius Gorgonio                                    |       ///\n");
+    printf("///           |     Ensinar Nos Meros Humanos                           |       ///\n");
+    printf("///           |     Armazenar Dados.                                    |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///           |                                                         |       ///\n");
+    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = = = = =        ///\n");
+    printf("///                                                                             ///\n");
+    printf("///                                                                             ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////////\n");
+} 
